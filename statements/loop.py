14132016108582f6ac6d21e38ff09c02358941e5
Howtoautomate.in.th <mart@howtoautomate.in.th>
2020-02-28 13:25:32 +0700

    Updated loop example

--- statements/loop.py
+++ statements/loop.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 def forloop():
     # traditionally for-loop
     for x in range(0, 3):
@@ -22,8 +25,31 @@ def forwithiterable():
     for x in collection:
         print(x)
 
+def loppenumerate():
+    word = 'abcde'
+    # instead of create running index use enumerate instead 
+    for index,letter in enumerate(word):
+        print(index)
+        print(letter)
+
+def ziplist():
+    # zip n lists into one list
+    # will zip only shortest list
+    number_list = [1,2,3,5,6,7,8,9]
+    string_list = ['a','b','c']
+    for n,s in zip(number_list,string_list):
+        print(n)
+        print(s)
+
 if __name__ == '__main__':
     forloop()
+    print('\n')
     whileloop()
+    print('\n')
     forelse()
+    print('\n')
     forwithiterable()
+    print('\n')
+    loppenumerate()
+    print('\n')
+    ziplist()
